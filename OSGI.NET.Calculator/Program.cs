414cc82c673ec8598b3e07773ff4982cf67daaba
daywrite <daywrite@yeah.net>
2014-07-08 10:24:20 +0800

    跑通

---
+++ OSGI.NET.Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIShell.OSGi;
+
+namespace OSGI.NET.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BundleRuntime bundleRuntime = new BundleRuntime())
+            {
+                bundleRuntime.Start();
+
+                //TODO
+
+                Console.WriteLine("Press enter to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ OSGI.NET.Calculator/bin/plugins/Calculator.DemoOne/Activator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIShell.OSGi;
+
+namespace Calculator.DemoOne
+{
+    public class Activator : IBundleActivator
+    {
+        public void Start(IBundleContext context)
+        {
+            //todo:
+            Console.WriteLine(string.Format("{0} 开始。", context.Bundle.SymbolicName));
+        }
+
+        public void Stop(IBundleContext context)
+        {
+            //todo:
+            Console.WriteLine(string.Format("{0} 停止。", context.Bundle.SymbolicName));
+        }
+    }
+}
