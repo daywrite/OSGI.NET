e0d1e8fda7788cbb3227f7327b653704f221d59e
daywrite <daywrite@yeah.net>
2014-07-08 11:39:42 +0800

    面向服务

--- OSGI.NET.Calculator/Program.cs
+++ OSGI.NET.Calculator/Program.cs
@@ -19,11 +19,15 @@ namespace OSGI.NET.Calculator
                 Console.WriteLine("Input the expression to be calculated:");
                 string expression = Console.ReadLine();
 
-                CD.ICalculate calculator = new CD.Calculator();
-                Console.WriteLine(string.Format("Calculate result of {0} is {1}.",
-                    expression,
-                    calculator.Calculate(expression)));
-
+                //CD.ICalculate calculator = new CD.Calculator();
+                List<CD.ICalculate> list = bundleRuntime.GetService<CD.ICalculate>();
+                foreach (CD.ICalculate calculator in list)
+                {
+                    Console.WriteLine(string.Format("{0}",calculator.GetType().FullName));
+                    Console.WriteLine(string.Format("Calculate result of {0} is {1}.",
+                        expression,
+                        calculator.Calculate(expression)));
+                }
 
                 Console.WriteLine("Press enter to exit...");
                 Console.ReadLine();
---
+++ OSGI.NET.Calculator/bin/plugins/Calculator.DemoTwo/Activator.cs
@@ -0,0 +1,24 @@
+﻿using Calculator.DemoOne;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIShell.OSGi;
+
+namespace Calculator.DemoTwo
+{
+    public class Activator : IBundleActivator
+    {
+        public void Start(IBundleContext context)
+        {
+            //todo:
+            Console.WriteLine(string.Format("{0} 开始。", context.Bundle.SymbolicName));
+            context.AddService<ICalculate>(new Calculator.DemoTwo.Impl.Calculator());
+        }
+
+        public void Stop(IBundleContext context)
+        {
+            //todo:
+            Console.WriteLine(string.Format("{0} 停止。", context.Bundle.SymbolicName));
+        }
+    }
+}
